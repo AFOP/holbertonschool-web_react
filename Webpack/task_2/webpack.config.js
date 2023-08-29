70318fe5888404104b26f3c93556f9154f652809
AFOP <felipe@ASUSAFOP.localdomain>
2023-08-29 13:26:57 -0500

    [ADD] task 2

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,45 +1,38 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
-  entry: './task_2/js/dashboard_main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
   mode: 'production',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './task_2/public/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'main.css',
-    }),
-    new ImageMinimizerPlugin({
-      minimizerOptions: {
-        plugins: [
-          ['jpegtran', { quality: 75, progressive: true }],
-          ['optipng', { optimizationLevel: 5 }],
-          ['svgo', { plugins: [{ removeViewBox: false }] }],
-        ],
-      },
-    }),
-  ],
+  entry: './js/dashboard_main.js',
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
+          'style-loader',
+          'css-loader'
+        ]
+      }, 
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
         ],
       },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-      },
-    ],
+    ]
   },
 };
