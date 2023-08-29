679191bebb9a2183f948f33db66bd270abfe6b10
AFOP <felipe@ASUSAFOP.localdomain>
2023-08-29 14:06:22 -0500

    [UPDATE] task 1

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,20 +1,17 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-const updateCounter = _.debounce(() => {
-  const countElement = $('#count');
-  let count = parseInt(countElement.text());
-  count++;
-  countElement.text(`${count} clicks on the button`);
-}, 300);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-$(document).ready(function() {
-  const body = $('body');
-  body.append('<p>Holberton Dashboard</p>');
-  body.append('<p>Dashboard data for the students</p>');
-  body.append('<button id="start-btn">Click here to get started</button>');
-  body.append('<p id="count"></p>');
-  body.append('<p>Copyright - Holberton School</p>');
+let count = 0;
 
-  $('#start-btn').click(updateCounter);
-});
+function updateCounter() { count++; }
+
+$('button').click(_.debounce(() => {
+  updateCounter();
+  $('#count').text(`${count} clicks on the button`);
+}));
