7dd3d30cb5fe97699cf5c43351276ffd4d481257
AFOP <felipe@ASUSAFOP.localdomain>
2023-09-18 21:54:20 -0500

    [ADD] task 3

--- react_props/task_3/dashboard/src/setupTests.js
+++
@@ -1,5 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
-
-// Configure Enzyme with the React 18 adapter
-configure({ adapter: new Adapter() });
