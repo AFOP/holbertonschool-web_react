339f89171ec975b050c9940e3adf96ea36e0cf71
AFOP <felipe@ASUSAFOP.localdomain>
2023-10-09 14:24:38 -0500

    [UPDATE] task 4

--- 0x08_react_redux_action_creator_normalizr/task_4/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_4/dashboard/src/schema/notifications.js
@@ -1,15 +1,30 @@
-import normalizedData from './notifications'; // Import the normalized data
+import * as notifications from '../notifications.json';
+import { normalize, schema } from 'normalizr';
 
-export function getAllNotificationsByUser(userId) {
-  const notifications = normalizedData.entities.notifications;
-  const userNotifications = [];
 
-  for (const notificationId in notifications) {
-    const notification = notifications[notificationId];
-    if (notification.author === userId) {
-      userNotifications.push(notification);
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, {
+  idAttribute: 'guid'
+});
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message
+});
+export const normalizedData = normalize(notifications.default, [notification]);
+
+const getAllNotificationsByUser = (userId) => {
+  const notiEntity = normalizedData.entities.notifications;
+  const messEntity = normalizedData.entities.messages;
+  const result = [];
+
+  for (let key in notiEntity) {
+    if (notiEntity[key].author === userId) {
+      const context = notiEntity[key].context;
+      result.push(messEntity[context]);
     }
   }
 
-  return userNotifications;
+  return result;
 }
+
+export default getAllNotificationsByUser;
