9c91df5e85c8598cb0a978ce3bc2735c8a70b769
AFOP <felipe@ASUSAFOP.localdomain>
2023-10-28 16:37:35 -0500

    [ADD] task 4

--- react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Map } from 'immutable';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import ReduxThunk from 'redux-thunk';
 
 import App from './App/App';
-import uiReducer, { initialState } from './reducers/uiReducer';
+import rootReducer, { initialState } from './reducers/rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(uiReducer, Map(initialState), composeEnhancers(applyMiddleware(ReduxThunk)));
+const store = createStore(combineReducers(rootReducer), initialState, composeEnhancers(applyMiddleware(ReduxThunk)));
 
 ReactDOM.render(
   <React.StrictMode>
