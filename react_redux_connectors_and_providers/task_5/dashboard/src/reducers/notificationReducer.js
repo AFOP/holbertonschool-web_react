f04b43cc76f492f9edbf66dda7b55f34d89c9d36
AFOP <felipe@ASUSAFOP.localdomain>
2023-10-28 16:55:35 -0500

    [ADD] task 5

--- react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
+++ react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
@@ -1,29 +1,46 @@
-import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from '../actions/notificationActionTypes';
-import { notificationsNormalizer } from '../schema/notifications';
+import { Map, fromJS } from "immutable";
 
-const { Map, setIn } = require('immutable');
+import {
+  FETCH_NOTIFICATIONS_SUCCESS,
+  MARK_AS_READ,
+  SET_TYPE_FILTER,
+  SET_LOADING_STATE,
+} from "../actions/notificationActionTypes";
 
-export const initialState = {
-  notifications: [],
+export const initialNotificationState = {
+  notifications: {},
   filter: "DEFAULT",
+  loading: false,
 };
 
-export const notificationReducer = (state = Map(initialState), action) => {
-  switch(action.type) {
+import notificationsNormalizer from "../schema/notifications";
+
+const notificationReducer = (state = Map(initialNotificationState), action) => {
+  switch (action.type) {
     case FETCH_NOTIFICATIONS_SUCCESS:
-      const data = notificationsNormalizer(action.data);
-      Object.keys(data.notifications).map((key) => {
-        data.notifications[key].isRead = false;
+      const normalizedData = notificationsNormalizer(action.data);
+
+      Object.keys(normalizedData.notifications).map((key) => {
+        normalizedData.notifications[key].isRead = false;
       });
-      return state.merge(data);
+
+      return state.mergeDeep(normalizedData);
+
     case MARK_AS_READ:
-      return setIn(state, ['notifications', String(action.index), 'isRead'], true);
+      return state.setIn(
+        ["notifications", String(action.index), "isRead"],
+        true
+      );
+
     case SET_TYPE_FILTER:
-      return state.set('filter', action.filter);
+      return state.set("filter", action.filter);
+
+    case SET_LOADING_STATE:
+      return state.set("loading", action.loading);
+
     default:
       break;
   }
-
   return state;
 };
 
